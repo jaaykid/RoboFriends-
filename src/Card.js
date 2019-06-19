aa70b8b051f1412067eb0c74cb05df5a18892cc1
jaay_kid <justincurrydev@gmail.com>
2019-06-19 12:45:47 -0700

    made api url call dynamic

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Card = (props) => {
   return (
     <div className="bg-light-green dib br3 pa3 ma2 nw2 grow shadow-5">
-      <img src="https://robohash.org/squad?200x200" alt="robots"/>
+      <img src={`https://robohash.org/${props.id}?200x200`} alt="robots"/>
       <div>
         <h2>{props.name}</h2>
         <p>{props.email}</p>
