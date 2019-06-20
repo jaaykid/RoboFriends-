a4ccf2a5daf3321c9c027f4c6e757e503fcefc6b
jaay_kid <justincurrydev@gmail.com>
2019-06-19 17:15:07 -0700

    added search functionality and SEGA font

---
+++ src/SearchBox.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const SearchBox = ({searchChange}) => {
+  console.log(searchChange)
+  return (
+    <div className="pa2">
+        <input className="pa3 ba b--green bg-lightest-blue" onChange={searchChange} type="search" placeholder="search robots"/>
+    </div>
+  );
+}
+
+
+export default SearchBox
