90be1295dcbb9f42218ab1080751d28ed9a7a830
jaay_kid <justincurrydev@gmail.com>
2019-06-19 18:08:24 -0700

    added stylig for scrolling

--- src/App.js
+++ src/App.js
@@ -1,36 +1,47 @@
 import React, { Component } from 'react'
 import CardList from './Cardlist'
 import SearchBox from './SearchBox'
-import { robots } from './robots'
+import Scroll from './scroll'
 import './App.css'
 
 
 class App extends Component {
-state = {
-  robots: robots, 
-  searchField: ' '
+constructor() {
+  super()
+  this.state = {
+    robots: [],
+    searchField: ' '
+  }
+}
+
+componentDidMount() {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(users => this.setState({robots: users}))
 }
 
 onSearchChange = (e) => {
   this.setState({
     searchField: e.target.value
   })
-  const filterRobots = this.state.robots.filter(robot => {
-    return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
-  })
-   console.log(filterRobots)
 }
 render () {
   const filterRobots = this.state.robots.filter(robot => {
     return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
   })
+  if(this.state.robots.length === 0 ) {
+    return <h1>Loading...</h1>
+  } else {
     return (
       <div className='tc'>
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filterRobots} />
+        <Scroll>
+          <CardList robots={filterRobots} />
+        </Scroll>
       </div>
     );
+  }
   }
 }
 
